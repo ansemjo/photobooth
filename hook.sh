28d939984f816ea9f31b1cde85bd9ac3ba69938c
Anton Semjonov <anton@semjonov.de>
2018-06-27 00:04:38 +0200

    move slideshow commands to hook script

--- hook.sh
+++ hook.sh
@@ -1,32 +1,60 @@
 #!/usr/bin/env bash
-self=$(basename $0)
 
-printf '%s %s\n' "$(date --utc +%FT%T%Z)" "EXECUTING HOOK"
+# fail on any error
+set -e
+
+# load functions
+. ./library.sh
 
 case "$ACTION" in
 
 	init)
-		echo "$self: INIT"
+
+		# log directory that is used
+		log "using $DIRECTORY"
+
+		# copy the frame that is displayed first
+		log "copy startscreen"
+		cp -f "$STARTSCREEN" "$DIRECTORY/"
+
 	;;
 
 	start)
-		echo "$self: START"
+
+		# start refreshing slideshow in the background
+		log "initialize background slideshow"
+		feh \
+			--hide-pointer \
+			--fullscreen \
+			--auto-zoom \
+			--randomize \
+			--reload 1 \
+			--slideshow-delay 1 \
+			"$DIRECTORY" &
+
 	;;
 
 	download)
-		echo "$self: DOWNLOAD to $ARGUMENT"
-		./slideshow.sh "$ARGUMENT" &
+
+		# display new photo on top
+		log "display $ARGUMENT"
+		feh \
+			--hide-pointer \
+			--fullscreen \
+			--auto-zoom \
+			--cycle-once \
+			--slideshow-delay 3 \
+			"$ARGUMENT" &
+
 	;;
 
 	stop)
-		echo "$self: STOP"
+
+		log "killing all remaining feh processes"
+		killall feh
+
 	;;
 
-	*)
-		echo "$self: Unknown action: $ACTION"
-		exit 1
-	;;
+	*) err "unknown action: $ACTION"; exit 1 ;;
 
 esac
-
-exit 0
--- photobooth.sh
+++ photobooth.sh
@@ -15,12 +15,6 @@ export DIRECTORY=$(newest-dir "$PHOTOBOOTH")
 # error if there is none yet, i.e. $DIRECTORY is empty
 [[ -n $DIRECTORY ]] || fatal "couldn't determine newest directory! is $PHOTOBOOTH empty?"
 
-# log directory that is used
-log "using $DIRECTORY"
-
-# copy start screen
-cp -f "$STARTSCREEN" "$DIRECTORY/"
-
 # start tethering
 gphoto2 \
   --capture-tethered \
